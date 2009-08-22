8bbc00348cf6434ed8613cb145c3166e758dfa29
itakingiteasy <itakingiteasy@c1b0e024-3177-11de-8a2e-13aa1706fec1>
2009-08-22 10:18:06 +0000

    ok
    git-svn-id: http://danbooru-v7sh-grabber.googlecode.com/svn/trunk@113 c1b0e024-3177-11de-8a2e-13aa1706fec1

--- grab.sh
+++ grab.sh
@@ -39,7 +39,7 @@
 # output:
 # side effect: defines variables
 init() {
-  g_version="Danbooru v7sh grabber v0.10.15 for Danbooru API v1.13.0";
+  g_version="Danbooru v7sh grabber v0.10.16 for Danbooru API v1.13.0";
 # const
   c_anonymous_tag_limit="2";      # API const
   c_registred_tag_limit="6";      # API const
@@ -1046,6 +1046,7 @@ init_danbooru() {
   # side effect:
   get_count() {
   (
+    persist="";
     if [ "${1-}" = "persist" ]; then
       persist="${1-}";
       shift;
